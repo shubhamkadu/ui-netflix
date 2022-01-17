20fb23cf658f7ad63fcde2996ef4dd2744fabe46
shubhamkadu <shubhampkadu@gmail.com>
2022-01-17 14:47:56 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import HomeScreen from "./HomeScreen";
+
+
+function App() {
+  
+    return ( 
+        <div className = "app">
+       
+            <HomeScreen/>
+                
+        </div>
+    );
+}
+
+export default App;
